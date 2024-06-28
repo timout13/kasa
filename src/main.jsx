a5583c53a6fe78efab51e03c58bbef32965c27c3
Timothe Lebrun <timothe463@icloud.com>
2024-06-28 08:53:32 +0200

    feat: import data

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/index.jsx'
 import About from './pages/About/about.jsx'
 import Announce from './pages/Announce/announce.jsx'
@@ -8,15 +8,18 @@ import Error404 from './pages/Error/404.jsx'
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Router>
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/a-propos" element={<About />}></Route>
-            <Route path="/location" element={<Announce />}></Route>
-            {/* ⬆️ path="/location/:idLocation" */}
-            <Route path="/*" element={<Error404 />}></Route>
-        </Routes>
-      </Router>
-  </React.StrictMode>,
+    //<React.StrictMode>
+    <>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/a-propos" element={<About/>}></Route>
+                <Route path="/location" element={<Announce/>}></Route>
+                {/* ⬆️ path="/location/:idLocation" */}
+                <Route path="/*" element={<Error404/>}></Route>
+            </Routes>
+        </Router>
+    </>
+    //</React.StrictMode>,
 )
+
