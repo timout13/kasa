9d76d8847de296ce131071a7adedb7a9145e836b
Timothe Lebrun <timothe463@icloud.com>
2024-07-19 15:35:06 +0200

    fix-collapse: integrate html component

--- src/components/collapse/index.jsx
+++ src/components/collapse/index.jsx
@@ -1,13 +1,31 @@
+import {useEffect, useRef, useState} from "react";
+
 function Collapse({title,children}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
+
+    const handleToggle = (e) => {
+        e.preventDefault();
+        setIsOpen((prevIsOpen) => !prevIsOpen);
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+        } else {
+            contentRef.current.style.height = '0px';
+        }
+    }, [isOpen]);
     return (
         <>
             <div className="collapse">
-                <details className="collapse-details">
+                <details className="collapse-details" open={isOpen == true ? true: null} onClick={(e)=>handleToggle(e)}>
                     <summary className="collapse-details-summary">
                         <span className="collapse-details-summary-title" role="term" aria-details="collapse-content">{title}</span>
                     </summary>
                 </details>
-                <div role="definition" id="collapse-content" className="collapse-content">
+                <div role="definition" id="collapse-content"  ref={contentRef}
+                     className={`collapse-content  ${isOpen ? 'collapse_content--open' : 'collapse_content--closed'}`}>
                     {children}
                 </div>
             </div>
